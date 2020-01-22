80845a59b5c55a636f5bd1968c37385ea56dbf75
David Montesdeoca <david.montesdeoca@bluetab.net>
2020-01-22 09:29:21 +0100

    Add SongList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SongList from "./SongList";
 
 export default class App extends React.Component {
   render() {
-    return <div>Hi from App</div>;
+    return (
+      <div>
+        <SongList />
+      </div>
+    );
   }
 }
